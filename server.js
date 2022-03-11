34b4c0d519391ef42949804d048715f92a4c9b64
EduardoZielke <83708846+EduardoZielke@users.noreply.github.com>
2022-03-11 11:51:50 -0300

    added-server-package

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('batata')
+const express = require('express')
+const app = express()
+const {resolve} = require('path')
+
+app.use('/', express.static(resolve(__dirname, './build')))
+
+app.listen(process.env.PORT || 5000, (err) => {
+    if(err) {return console.log(err)}
+
+    console.log('Servidor Front-end rodando!')
+})
