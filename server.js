c00dab067f2d8a838c91016f5052eab03d1ad5fd
EduardoZielke <83708846+EduardoZielke@users.noreply.github.com>
2022-03-12 01:37:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {resolve} = require('path')
 
-app.use('/*', express.static(resolve(__dirname, './build/index.html')))
+app.use('*', express.static(resolve(__dirname, './build/index.html')))
 
 app.listen(process.env.PORT || 5000, (err) => {
     if(err) {return console.log(err)}
